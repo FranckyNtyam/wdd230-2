2cfd5b89f76204e61b5be590c6ab95ae59815acf
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-04-23 01:37:35 +0100

    Create my JavaScript file.

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+let day = new Date(document.lastModified);
+let year = day.getFullYear();
+let time = day.getHours() + ":" + day.getMinutes() + ":" + day.getSeconds();
+let date = day.getMonth() +1  + "/" + day.getDate() +"/" + day.getFullYear();
+document.getElementById('year').innerHTML = year;
+document.getElementById('last-modified').innerHTML = date + " " + time;
