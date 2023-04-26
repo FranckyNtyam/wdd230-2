0e3b26bf6c629dd2fef1a77cee09824de0a4cc22
franckui <francky.ntyam@gmail.com>
2023-04-26 08:07:45 +0100

    update my file

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@ let day = new Date(document.lastModified);
 let year = day.getFullYear();
 let time = day.getHours() + ":" + day.getMinutes() + ":" + day.getSeconds();
 let date = day.getMonth() +1  + "/" + day.getDate() +"/" + day.getFullYear();
-document.getElementById('year').innerHTML = year;
-document.getElementById('last-modified').innerHTML = date + " " + time;
+document.querySelector('year').innerHTML = year;
+document.querySelector('last-modified').innerHTML = date + " " + time;
