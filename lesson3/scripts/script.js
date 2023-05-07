d7c9e3dc7938f7428c90825d6c02819097447a40
franckui <francky.ntyam@gmail.com>
2023-05-07 01:48:28 +0100

    modification of files

--- lesson3/scripts/script.js
+++ lesson3/scripts/script.js
@@ -1,6 +1,6 @@
 let day = new Date(document.lastModified);
 let year = day.getFullYear();
 let time = day.getHours() + ":" + day.getMinutes() + ":" + day.getSeconds();
-let date = (day.getMonth() + 1) + "/" + day.getDate() + "/" + day.getFullYear();
-document.getElementById('year').innerHTML = year;
-document.getElementById('current-date').innerHTML = date + " " + time;
+let date = day.getMonth() +1  + "/" + day.getDate() +"/" + day.getFullYear();
+document.querySelector('#year').innerHTML = year;
+document.querySelector('#last-modified').innerHTML = date + " " + time;
